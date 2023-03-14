95d48dcf6ef498047f128dc2ac87ef5ecdc4a218
Ricardo Chacon <ricardo@fama.io>
2023-03-14 00:22:25 -0600

    add JAVA Abstract Class

--- JAVA OOP/JAVA inheritance 1.java
+++ JAVA OOP/JAVA inheritance 1.java
@@ -3,29 +3,32 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Solution {
 
     public static void main(String[] args) {
-         class Animal{
-            void walk(){
+        class Animal {
+            void walk() {
                 System.out.println("I am walking");
             }
         }
- class Bird extends Animal {
+        class Bird extends Animal {
             void fly() {
                 System.out.println("I am flying");
             }
+
             void sing() {
                 System.out.println("I am singing");
             }
         }
 
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-            Bird birdie = new Bird();
-            birdie.walk();
-            birdie.fly();
-            birdie.sing();
-        }
-
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Bird birdie = new Bird();
+        birdie.walk();
+        birdie.fly();
+        birdie.sing();
     }
+
+}
